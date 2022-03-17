bdd79ebcbe53c063c3a44e66d7926ef5178f2812
Krasilnikov30 <salarovsmv@gmail.com>
2022-03-17 18:39:03 +0300

    Создал проект для тестирования Vector_basis

---
+++ lab2/Vector_basis/Vector_basis/Vector_basis_tests/Vector_basis_tests.cpp
@@ -0,0 +1,11 @@
+// Vector_basis_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "../../../../catch.hpp"
+
+TEST_CASE("some test")
+{
+	REQUIRE(2 * 2 == 4);
+}
