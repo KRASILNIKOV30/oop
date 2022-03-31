68397c7c27af51561c8a48ba28eabffd6001227a
Krasilnikov30 <bkrasilnikov3006@gmail.com>
2022-03-31 18:44:38 +0300

    Доделал структуру кода

--- lab2/HTMLDecoder/HTMLDecoder/HTMLDecode.cpp
+++ lab2/HTMLDecoder/HTMLDecoder/HTMLDecode.cpp
@@ -1,24 +1,49 @@
-
 #include <string>
 #include <iostream>
 #include "HTMLDecode.h"
 
+std::string SpecialChDef(std::string htmlEntity)
+{
+	if (htmlEntity == "&quot") { return """"; }
+	if (htmlEntity == "&apos") { return "''"; }
+	if (htmlEntity == "&lt") { return "<"; }
+	if (htmlEntity == "&gt") { return ">"; }
+	if (htmlEntity == "&amp") { return "amp"; }
+	return "0";
+}
+
 std::string HtmlDecode(std::string const& html)
 {
-	int index = 0;
-	while (html[index] != '\0')
+	std::string text = html;
+	std::string specialCh = "";
+	std::string htmlEntity = "";
+	for (int index = 0; index != -1 && text[index] != '\0'; index++)
 	{
-		index = html.find("&", index);
-		if (index != std::string::npos)
+		std::cout << index << std::endl;
+		index = text.find("&", index);
+		std::cout << index << std::endl;
+		if (index != -1)
 		{
-			std::string htmlEntity = "";
-			while (html[index] != ';' && html[index] != '\0')
+			//std::cout << index << " " << html[index] << std::endl;
+			int i = index;
+			while (text[i] != ';' && text[i] != '\0')
 			{
-				htmlEntity.push_back(html[index]);
-				index++;
+				htmlEntity.push_back(text[i]);
+				i++;
 			}
-			return htmlEntity;
-		};
-		return "HTML-entity was not found";
+			if (text[i] == ';')
+			{
+				specialCh = SpecialChDef(htmlEntity);
+				if (specialCh != "0")
+				{
+					text.replace(index, htmlEntity.size() + 1, specialCh);
+				}
+			}
+		}
+		else
+		{
+			break;
+		}
 	}
+	return text;
 }
--- lab2/HTMLDecoder/HTMLDecoder_tests/HTMLDecoder_tests.cpp
+++ lab2/HTMLDecoder/HTMLDecoder_tests/HTMLDecoder_tests.cpp
@@ -4,5 +4,10 @@
 
 TEST_CASE("Correct input")
 {
-	REQUIRE(HtmlDecode("Cat &lt; says &gt; &quot; Meow &quot;.M &amp; M &apos; s") == "&lt");
+	REQUIRE(HtmlDecode("Cat &lt; says &gt; &quot; Meow &quot;.M &amp; M &apos; s") == "Cat <says> ""Meow"". M&M’s ");
 }
+
+//TEST_CASE("Input without html-entities")
+//{
+//	REQUIRE(HtmlDecode("Cat <says> ""Meow"". M&M’s ") == "HTML-entity was not found");
+//}
