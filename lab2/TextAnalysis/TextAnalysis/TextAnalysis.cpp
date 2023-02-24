e379a68ef0e278f22067116c33485f3ec0f8d442
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-02-24 14:09:34 +0300

    Небольшой рефакторинг TextAnalysis

--- lab2/TextAnalysis/TextAnalysis/TextAnalysis.cpp
+++ lab2/TextAnalysis/TextAnalysis/TextAnalysis.cpp
@@ -12,23 +12,23 @@ namespace {
 }
 
 void TextAnalysis(std::istream &input) {
-    std::map<std::string, int> map;
+    std::map<std::string, int> wordsMap;
     std::map<std::string, int>::iterator iterator;
     std::string word;
     while (input >> word) {
         word = str_tolower(word);
-        iterator = map.find(word);
-        if (iterator != map.end()) {
+        iterator = wordsMap.find(word);
+        if (iterator != wordsMap.end()) {
             iterator->second++;
         } else {
-            map.insert({ word, 1 });
+            wordsMap.insert({ word, 1 });
         }
         if (input.eof()) {
             break;
         }
     }
 
-    for (iterator = map.begin(); iterator != map.end(); ++iterator) {
+    for (iterator = wordsMap.begin(); iterator != wordsMap.end(); ++iterator) {
         std::cout << iterator->first << ' ' << iterator->second << std::endl;
     }
 }
