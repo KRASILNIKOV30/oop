a1d346b91c1dbfdb9144ab7cccf009fe0c6436ef
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-05-09 13:46:10 +0300

    Дописал интерфейс фигуры

--- lab4/Shapes/Shapes/IShape.h
+++ lab4/Shapes/Shapes/IShape.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <cstdint>
 
 class IShape
 {
 public:
 	virtual double GetArea() const;
 	virtual double GetPerimeter() const;
-
+	virtual double ToString() const;
+	virtual uint32_t GetOutlineColor() const;
 };
