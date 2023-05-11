c2afe96a984e5a1ba868f3061c3f7214c2a7ca2e
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-05-11 19:08:51 +0300

    Начал добавлять визцализацию

--- lab4/ShapesVizualization/ShapesVizualization/ShapesVizualization.cpp
+++ lab4/ShapesVizualization/ShapesVizualization/ShapesVizualization.cpp
@@ -1,47 +1,104 @@
 ﻿#include <SDL2/SDL.h>
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-#undef main
+#include <iostream>
+#undef main;
 
-int main(int argc, char** args) {
+using namespace std;
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        return 1;
-    }
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
 
-    SDL_Surface* screen_surface = NULL;
+SDL_Window* win = NULL;
+SDL_Renderer* ren = NULL;
 
-    SDL_Window* window = NULL;
-
-    window = SDL_CreateWindow("Hello, SDL 2!", SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN);
-
-    if (window == NULL) {
-        return 1;
-    }
-
-    screen_surface = SDL_GetWindowSurface(window);
-
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 255, 255, 255));
-
-    SDL_Rect r;
-    r.x = 50;
-    r.y = 50;
-    r.w = 50;
-    r.h = 50;
-
-    SDL_FillRect(screen_surface, &r, SDL_MapRGB(screen_surface->format, 0, 0, 0));
-
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Delay(2000);
-
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    return 0;
+class CPoint
+{
+public:
+    double x, y;
 };
+
+bool init() {
+    bool ok = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        cout << "Can't init SDL: " << SDL_GetError() << endl;
+    }
+
+    win = SDL_CreateWindow("Примитивы", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        cout << "Can't create window: " << SDL_GetError() << endl;
+        ok = false;
+    }
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (ren == NULL) {
+        cout << "Can't create renderer: " << SDL_GetError() << endl;
+        ok = false;
+    }
+    return ok;
+}
+
+void quit() {
+    SDL_DestroyWindow(win);
+    win = NULL;
+
+    SDL_DestroyRenderer(ren);
+    ren = NULL;
+
+    SDL_Quit;
+}
+
+int main(int arhc, char** argv) {
+    if (!init()) {
+        quit();
+        system("pause");
+        return 1;
+    }
+
+    CPoint vertex1 = { 10, 10 };
+    CPoint vertex2 = { 90, 30 };
+    CPoint vertex3 = { 10, 90 };
+
+    SDL_Vertex triangleVertex[3] =
+    {
+     {
+      { vertex1.x, vertex1.y }, 
+      { 0, 200, 0, 0xFF },
+      { 0.f, 0.f }
+     },
+     {
+      { vertex2.x, vertex2.y },
+      { 0, 200, 0, 0xFF },
+      { 0.f, 0.f }
+     },
+     {
+      { vertex3.x, vertex3.y },
+      { 0, 200, 0, 0xFF },
+      { 0.f, 0.f }
+     }
+    };
+
+    SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(ren);
+    
+
+    /*SDL_Rect rect1 = { 10, 10, 50, 50 };
+    SDL_RenderFillRect(ren, &rect1);
+
+    SDL_Rect rect2 = { 70, 10, 50, 50 };
+    SDL_RenderDrawRect(ren, &rect2);*/
+
+    SDL_RenderGeometry(ren, NULL, triangleVertex, 3, NULL, 0);
+
+    SDL_SetRenderDrawColor(ren, 0xff, 0x00, 0x00, 0xFF);
+
+    SDL_RenderDrawLine(ren, vertex1.x, vertex1.y, vertex2.x, vertex2.y);
+    SDL_RenderDrawLine(ren, vertex2.x, vertex2.y, vertex3.x, vertex3.y);
+    SDL_RenderDrawLine(ren, vertex3.x, vertex3.y, vertex1.x, vertex1.y);
+
+    SDL_RenderPresent(ren);
+
+    SDL_Delay(5000);
+
+    quit();
+    return 0;
+}
