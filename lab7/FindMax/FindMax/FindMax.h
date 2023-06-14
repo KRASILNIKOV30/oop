01515b8b7065056064bb6d6da4a516ffede11c93
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-06-14 14:15:03 +0300

    Реализовал функцию FindMax

---
+++ lab7/FindMax/FindMax/FindMax.h
@@ -0,0 +1,13 @@
+#include <vector>
+
+template<typename T, typename Less>
+bool FindMax(std::vector<T> const& arr, T& maxValue, Less const& less)
+{
+	auto it = std::max_element(arr.begin(), arr.end(), less);
+	if (it == arr.end())
+	{
+		return false;
+	}
+	maxValue = *it;
+	return true;
+};
---
+++ lab7/FindMax/FindMaxTests/FindMaxTests.cpp
@@ -0,0 +1,78 @@
+﻿#include "../../../external/catch2/catch.hpp"
+#include "../FindMax/FindMax.h"
+
+TEST_CASE("Find max in empty array")
+{
+	std::vector<int> v;
+	int max;
+	CHECK_FALSE(FindMax(v, max, [](int a, int b) { return a < b;  }));
+}
+
+SCENARIO("Find max in array with one value")
+{
+	GIVEN("Array with ine value")
+	{
+		std::vector<int> v{ 5 };
+
+		WHEN("Find max in array with one value")
+		{
+			int max;
+			CHECK(FindMax(v, max, [](int a, int b) { return a < b;  }));
+
+			THEN("Max is equal to value in array")
+			{
+				CHECK(max == 5);
+			}
+		}
+	}
+}
+
+SCENARIO("Find max in array of sportsmen")
+{
+	GIVEN("Array of three sportsmen")
+	{
+		struct Sportsman
+		{
+			std::string name;
+			int height;
+			int weight;
+		};
+		std::vector<Sportsman> sportsmen
+		{
+			{ "Vasya", 175, 85 },
+			{ "Ivan", 190, 80 },
+			{ "Petya", 170, 65 }
+		};
+
+		WHEN("Find sportsman with max height")
+		{
+			Sportsman maxHeight;
+			CHECK(FindMax(sportsmen, maxHeight, [](Sportsman a, Sportsman b) { return a.height < b.height; }));
+
+			THEN("Sportsman with max height is Ivan")
+			{
+				CHECK(maxHeight.name == "Ivan");
+			}
+		}
+
+		WHEN("Find sportsman with max weight")
+		{
+			Sportsman maxWeight;
+			CHECK(FindMax(sportsmen, maxWeight, [](Sportsman a, Sportsman b) { return a.weight < b.weight; }));
+
+			THEN("Sportsman with max weight is Vasya")
+			{
+				CHECK(maxWeight.name == "Vasya");
+			}
+		}
+
+	}
+}
+
+TEST_CASE("Check commit-or-rollback behavior")
+{
+	std::vector<int> v{ 2, 5 };
+	int max = 1;
+	CHECK_THROWS(FindMax(v, max, [](int a, int b) { throw std::exception{ "" }; return true;  }));
+	CHECK(max == 1);
+}
